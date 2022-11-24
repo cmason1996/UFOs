a5772aacee60bd909e9faca07fb64637de1088b6
cmason1996 <astronomy747@yahoo.com>
2022-11-23 18:15:58 -0800

    Adding updated app.js file

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,16 @@
 const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody")
+// Begin the function for building the table the data will live in by making sure the data is reset to original after each use
+function buildTable(data) {
+    tbody.html("");
+}
+// Adding the forEach function to loop through all of the rows data.js and append a new row of cells for each object in the data
+data.forEach((dataRow) => {
+    let row = tbody.append("tr");
+    Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+    );
+});
